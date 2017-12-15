b958989a0e4b4bc355877d3359bca13390605841
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2017-12-15 15:28:26 -0500

    working on bubble sort

--- DMOJ/COCI '08 Contest 4 #1 Mjehuric.py
+++ DMOJ/COCI '08 Contest 4 #1 Mjehuric.py
@@ -5,7 +5,11 @@ l1.append(input)
 l1.append(input)
 li.append(input)
 def bubble(alist):
-    for i in range len(l1) -1:
-        
+    for i in range (alist(l1) -1):
+        if alist[i]>alist[i+1]:
+            temp = alist[i]
+            alist[i] = alist[i+1]
+            alist[i+1] = temp
+        print l1     
 
    
