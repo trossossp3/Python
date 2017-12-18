7fba3074e3a64173c36302da76d2e8bd6cc5ee59
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2017-12-18 08:54:16 -0500

    trying to fix some syntax errors

--- DMOJ/COCI '08 Contest 4 #1 Mjehuric.py
+++ DMOJ/COCI '08 Contest 4 #1 Mjehuric.py
@@ -1,15 +1,16 @@
-l1=[]
-l1.append(input)
-l1.append(input)
-l1.append(input)
-l1.append(input)
-li.append(input)
-def bubble(alist):
-    for i in range (alist(l1) -1):
-        if alist[i]>alist[i+1]:
-            temp = alist[i]
-            alist[i] = alist[i+1]
-            alist[i+1] = temp
-        print l1     
+alist = raw_input('').rstrip()
+alist = alist.split(' ')
 
+def bubble (alist):
+   # if alist = [1,2,3,4,5]:
+       # print alist
+    #
+        for i in range (0, len(alist) -1):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+print alist    
+
+bubble(alist)
    
