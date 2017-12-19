a9f904c9a9c4020d5d5e0da6e7e6aabdccc87545
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2017-12-19 08:59:46 -0500

    finaly finished the bubble sort dmoj question

--- DMOJ/COCI '08 Contest 4 #1 Mjehuric.py
+++ DMOJ/COCI '08 Contest 4 #1 Mjehuric.py
@@ -1,16 +1,18 @@
-alist = raw_input('').rstrip()
-alist = alist.split(' ')
-
-def bubble (alist):
-   # if alist = [1,2,3,4,5]:
-       # print alist
-    #
-        for i in range (0, len(alist) -1):
+def bubbleSort(alist):
+    for i in range(len(alist)-1,0,-1):
+        for i in range(i):
             if alist[i]>alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
-print alist    
+            print(alist)
 
-bubble(alist)
-   
+alist = raw_input('').rstrip()
+alist = alist.split(' ')
+
+bubbleSort(alist)
+#print(alist)
+
+
+
+
