d0c3f0f116df147fee250fc3d47c1c35bb09cc08
trossossp3 <21trossos@gmail.com>
2018-12-29 11:33:41 -0800

    did some opencv

--- plateTrack.py
+++ plateTrack.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2 as cv
-face_cascade = cv.CascadeClassifier('Z:\Dev\Anaconda\pkgs\libopencv-3.4.1-h875b8b8_3\Library\etc\haarcascades\haarcascade_frontalface_default.xml')
+face_cascade = cv.CascadeClassifier('Z:\Dev\Anaconda\pkgs\libopencv-3.4.1-h875b8b8_3\Library\etc\haarcascades\haarcascade_russian_plate_number.xml')
 #eye_cascade = cv.CascadeClassifier('Z:\Dev\Anaconda\pkgs\libopencv-3.4.1-h875b8b8_3\Library\etc\haarcascades\haarcascade_eye.xml')
-img = cv.imread('face.jpg')
+img = cv.imread('test.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
+#russian plates work best
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv.rectangle(img,(x,y),(x+w,y+h),(0, 100, 0),5)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     #eyes = eye_cascade.detectMultiScale(roi_gray)
